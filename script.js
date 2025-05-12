b139105e7bd5753689f0778b4fcd286d087fa36f
Fork (Liam) <liamjones989@outlook.com>
2025-05-12 09:48:10 -0500

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const map = L.map('map').setView([20, 0], 2);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
+jetData.forEach(jet => {
+  L.marker(jet.location)
+    .addTo(map)
+    .bindPopup(`
+      <b>${jet.owner}</b><br>
+      ${jet.model}<br>
+      <em>${jet.fact}</em>
+    `);
+});
 
