b714a8d8593950f38740b0c7b3ec65f9dc6a4fa4
Fork (Liam) <liamjones989@outlook.com>
2025-05-12 09:47:49 -0500

    data.js

--- data.js
+++ data.js
@@ -1,1 +1,21 @@
+const jetData = [
+  {
+    owner: "Taylor Swift",
+    model: "Dassault Falcon 900",
+    location: [33.6407, -84.4277],
+    fact: "Short hops during her 2023 tour."
+  },
+  {
+    owner: "Elon Musk",
+    model: "Gulfstream G650ER",
+    location: [25.7933, -80.2906],
+    fact: "Range: 7,500 miles."
+  },
+  {
+    owner: "Drake",
+    model: "Boeing 767 'Air Drake'",
+    location: [43.6777, -79.6248],
+    fact: "Converted cargo jet gifted by CargoJet."
+  }
+];
 
